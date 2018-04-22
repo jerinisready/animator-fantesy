f022384bec57a234eaf16e2990a84f8dd6201815
Jerin <jerinisready@gmail.com>
2018-04-22 20:14:44 +0530

    My Alt changes!

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     console.log('Ready!')
 });
+
+window.onscroll = function() {scroll_marker(); };
+
+function scroll_marker() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
